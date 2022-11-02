5a2f137b90a8676a1b20b04c19fb8328d32412ab
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-11-02 14:02:43 +0530

    added redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://hackmd.io/@NQP0NSuHQSqLw6DuqCWF6w/HyW4oGKVs",
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
